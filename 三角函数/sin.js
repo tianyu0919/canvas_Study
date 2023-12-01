958ceacf8d923033767cae6f596978763a432c7b
卢天宇 <1141049271@qq.com>
2023-12-02 01:08:14 +0800

    feat: 新增公共函数

--- 三角函数/sin.js
+++ 三角函数/sin.js
@@ -1,57 +1,6 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-const debounce = (fn, ms = 200) => {
-  // 防抖函数
-  let timer;
-  return (...args) => {
-    if (timer) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(() => {
-      fn.apply(this, args);
-    }, ms);
-  };
-};
-
-(() => {
-  function init() {
-    // 初始化函数
-    const ratio = window.devicePixelRatio; // 设备像素比
-    const w = window.innerWidth * ratio; // 窗口宽度
-    const h = window.innerHeight * ratio; // 窗口高度
-
-    canvas.width = w; // 设置canvas宽度
-    canvas.height = h; // 设置canvas高度
-    ctx.scale(ratio, ratio); // 缩放canvas
-    canvas.style.cssText = `width: ${w}px; height: ${h}px;`; // 设置canvas样式
-
-    let x = 0;
-    let y = 0;
-
-    ctx.strokeStyle = "#ccc";
-
-    while(x < w) {
-      ctx.beginPath();
-      ctx.moveTo(x, 0);
-      ctx.lineTo(x, h);
-      ctx.stroke();
-      ctx.strokeText(x.toFixed(0), x, 10)
-      x += 25;
-    }
-    while(y < h) {
-      ctx.beginPath();
-      ctx.moveTo(0, y);
-      ctx.lineTo(w, y);
-      ctx.stroke();
-      ctx.strokeText(y.toFixed(0), 0, y)
-      y += 25;
-    }
-  }
-  init();
-  window.addEventListener("resize", debounce(init)); // 监听窗口大小变化事件
-})();
-
 ctx.beginPath();
 ctx.moveTo(100, 100);
 ctx.arcTo(150, 100, 100, 150, 50);
@@ -64,8 +13,14 @@ ctx.strokeStyle = "blue";
 ctx.fill();
 ctx.stroke();
 
+ctx.beginPath();
+ctx.arc(300, 300, 50, 0, (Math.PI / 180) * 180, false);
+ctx.fill();
 
 ctx.beginPath();
-
-ctx.arc(300, 300, 50, 0, (Math.PI / 180) * 180, false);
+ctx.arc(75, 50, 25, 0, Math.PI / 180 * 110);
+ctx.moveTo(100,50)
+ctx.lineTo(75, 50)
+ctx.lineTo(75, 75);
+ctx.closePath()
 ctx.fill()
